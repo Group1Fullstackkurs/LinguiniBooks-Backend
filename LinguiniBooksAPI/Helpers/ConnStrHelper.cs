201affdb9b52843d9ff9eff07b791673a9b500ad
01001100 01001100 <leon.larsson@gmail.com>
2022-09-12 15:23:11 +0200

    connstr changes

--- LinguiniBooksAPI/Helpers/ConnStrHelper.cs
+++ LinguiniBooksAPI/Helpers/ConnStrHelper.cs
@@ -8,7 +8,7 @@
             {
                 return File.ReadAllText(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\linguiniConnStr.txt");
             } else {
-                return Environment.GetEnvironmentVariable("CONNSTR") ?? string.Empty;
+                return Environment.GetEnvironmentVariable("CONNSTR") ?? Environment.GetEnvironmentVariable("CONNSTR_MONGODB") ?? string.Empty;
             }
         }
     }
