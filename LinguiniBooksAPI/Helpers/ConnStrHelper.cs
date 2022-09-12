a8ad378f23a59db73c45a7ac275d5fa380631c61
01001100 01001100 <leon.larsson@gmail.com>
2022-09-12 14:04:11 +0200

    yolo?

--- LinguiniBooksAPI/Helpers/ConnStrHelper.cs
+++ LinguiniBooksAPI/Helpers/ConnStrHelper.cs
@@ -4,16 +4,12 @@
     {
         public static string ReadConnStr()
         {
-            try
+            if (File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)))
             {
-                return File.ReadAllText((System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\linguiniConnStr.txt"));
+                return File.ReadAllText(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\linguiniConnStr.txt");
+            } else {
+                return Environment.GetEnvironmentVariable("CONNSTR") ?? string.Empty;
             }
-            catch(Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-            }
-            return String.Empty;
-
         }
     }
 }
