706c66e2654cc5a08b5306c48d35886b3f4512e5
T-junctions <leon.larsson@gmail.com>
2022-09-12 09:41:11 +0200

    123

--- DBLib/Models/BookModel.cs
+++ DBLib/Models/BookModel.cs
@@ -9,7 +9,7 @@ namespace DBDataAccess.Models
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
 
-        // Strings 
+        // Strings
         public string Id { get; set; } = string.Empty;
         public string first_name { get; set; } = string.Empty;
         public string last_name { get; set; } = string.Empty;
