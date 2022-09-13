cf8a3be87c70f69f3e639d0655d60b529846f005
T-junctions <leon.larsson@gmail.com>
2022-09-13 11:49:47 +0200

    Changes how the connStrHelper works

--- LinguiniBooksAPI/Helpers/ConnStrHelper.cs
+++ LinguiniBooksAPI/Helpers/ConnStrHelper.cs
@@ -4,16 +4,28 @@
     {
         public static string ReadConnStr()
         {
-            var connStr = Environment.GetEnvironmentVariable("CONNSTR");
+            var connStrDocs = string.Empty;
+            var connStrGit = Environment.GetEnvironmentVariable("CONNSTR");
             var connStrAzure = Environment.GetEnvironmentVariable("CONNSTR_MONGODB");
 
-            if(File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\linguiniConnStr.txt")) 
+            // HACK: It is made in this specific way for a specific reason, do not change! @Leon-Hobby
+            try
             {
-                File.ReadAllText(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\linguiniConnStr.txt");
-            } else if(connStr != "" && connStr != null)
+                connStrDocs = File.ReadAllText(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\linguiniConnStr.txt");
+            }
+            catch(Exception ex)
             {
-                return connStr;
-            } else if (connStrAzure != "" && connStrAzure != null){
+                Console.WriteLine(ex.ToString());
+            }
+
+            if(!string.IsNullOrEmpty(connStrDocs))
+            {
+                return connStrDocs;
+            } else if(!string.IsNullOrEmpty(connStrGit))
+            {
+                return connStrGit;
+            } else if (!string.IsNullOrEmpty(connStrAzure))
+            {
                 return connStrAzure;
             }
             return string.Empty;
