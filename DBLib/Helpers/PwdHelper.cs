d036e07dfb3161c4c049be216b514c58f85da73e
CGDM101 <51097129+CGDM101@users.noreply.github.com>
2022-09-21 14:38:48 +0200

    Added XML-comments for PasswordHelper.cs

--- DBLib/Helpers/PwdHelper.cs
+++ DBLib/Helpers/PwdHelper.cs
@@ -12,11 +12,23 @@ namespace DBDataAccess.Helpers
 {
     internal class PwdHelper
     {
+        /// <summary>
+        /// Checks if password is valid.
+        /// </summary>
+        /// <param name="user">The user whose password is to be checked.</param>
+        /// <param name="pwd">The password of the user</param>
+        /// <returns>A bool</returns>
         public bool IsPwdValid(UserModel user, string pwd)
         {
             return user.Hash == GetSaltedHash(pwd, user.Salt);
         }
 
+        /// <summary>
+        /// Salts the hash for extra security.
+        /// </summary>
+        /// <param name="pwd">The password to be salted.</param>
+        /// <param name="salt">The salt for the password</param>
+        /// <returns>A string with the salted password</returns>
         public string GetSaltedHash(string pwd, string salt)
         {
             byte[] bytes = Encoding.UTF8.GetBytes(pwd + salt);
@@ -26,6 +38,11 @@ namespace DBDataAccess.Helpers
             return Convert.ToBase64String(hash);
         }
 
+        /// <summary>
+        /// Gets salt to use on hashed password.
+        /// </summary>
+        /// <param name="size">Size of the salt.</param>
+        /// <returns>A string with the salt to use on password.</returns>
         public string GetSalt(int size = 32)
         {
             RandomNumberGenerator rng = RandomNumberGenerator.Create();
