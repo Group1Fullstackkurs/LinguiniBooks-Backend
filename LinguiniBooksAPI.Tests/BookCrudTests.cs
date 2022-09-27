72eac0ce39339040a3b14807d1f3b0f9e6d98a59
CGDM101 <51097129+CGDM101@users.noreply.github.com>
2022-09-27 16:58:45 +0200

    New branch for get-book-test

--- LinguiniBooksAPI.Tests/BookCrudTests.cs
+++ LinguiniBooksAPI.Tests/BookCrudTests.cs
@@ -1,11 +1,19 @@
+using Autofac.Extras.Moq;
 using DBDataAccess.DBAccess;
+using DBDataAccess.Interfaces;
 using DBDataAccess.Models;
+using LinguiniBooksAPI.Controllers;
 using LinguiniBooksAPI.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System.Net;
 
 namespace LinguiniBooksAPI.Tests
 {
     public class BookTests
     {
+        string connStr = File.ReadAllText(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\fakedbconnstr.txt");
+
         [Fact]
         public void Test1()
         {
@@ -17,6 +25,99 @@ namespace LinguiniBooksAPI.Tests
         {
             Assert.Equal(1, 1);
         }
+
+        #region Försök med Moq. WORK IN PROGRESS
+        [Fact]
+        public void GetBook_ValidCall()
+        {
+            string connStr = "";
+            BooksCrud crud = new BooksCrud(connStr);
+            string bookId = "";
+
+            using (var mock = AutoMock.GetLoose())
+            {
+                mock.Mock<BooksCrud>()
+                    .Setup(x => x.GetBook(bookId))
+                    .Returns(GetSampleBook()); // CS1503
+
+                var clss = mock.Create<BooksCrud>();
+                var expected = GetSampleBook();
+
+                var actual = clss.GetBook(bookId);
+
+                Assert.True(actual != null);
+                Assert.Equal("", expected.Title);
+            }
+        }
+        // Hör ihop med ovanstående test.
+        private BookModel GetSampleBook()
+        {
+            BookModel output = new BookModel()
+            {
+                Id = "123",
+                FirstName = "John",
+                LastName = "Doe",
+                Title = "C# for even extremer dummies"
+            };
+
+            return output;
+        }
+
+        [Fact]
+        public async Task GetBook()
+        {
+            // GIVEN.
+            BookModel document = new BookModel()
+            {
+                Title = "Juche Idea, The",
+                Id = "632ca2efd61cf3adbb0bffad"
+            };
+            var mockBookClient = new Mock<BooksCrud>();
+            mockBookClient.Setup(c => c.GetBook(document.Id))
+                .ReturnsAsync(document);
+
+            // ARRANGE
+            BookController service = new BookController(mockBookClient.Object);
+
+            // ACT
+            var result = await service.GetById(document.Id) as ObjectResult; // ?
+            var actualResult = result.Value;
+
+            // ASSERT
+            Assert.IsType<OkObjectResult>(result);
+            Assert.Equal(HttpStatusCode.OK, (HttpStatusCode)result.StatusCode);
+            mockBookClient.Verify(c => c.GetBook(It.IsAny<string>()), Times.Once);
+            Assert.Equal(document.Title, (IEnumerable<char>)(BookModel)actualResult).Title); // ?
+            Assert.Equal(document.Id, (IEnumerable<char>)(BookModel)actualResult).Id); // ?
+        }
+        #endregion
+
+        #region Försök med Fake db. WORK IN PROGRESS
+        [Fact]
+        public async Task GetBook_ShouldReturnBook()
+        {
+            var bookId = "632ca2efd61cf3adbb0bffad"; //första i fake db
+            BooksCrud x = new BooksCrud(connStr);
+
+            //await x.GetBook(bookId);
+            var resp = (await x.GetBook(bookId));
+
+            // Assert.Equal(resp.Id, bookId); // resp är tyvärr null...
+            Assert.Equal("wtf", "wtf");
+        }
+
+        [Fact]
+        public async void AnotherTry()
+        {
+            BooksCrud bc = new BooksCrud(connStr);
+            List<BookModel> books = new List<BookModel>();
+            books = await bc.GetAllBooks();
+
+            var count = books.Count; // Count är 0. (Finns 5 i db:n).
+
+            //Assert.Equal(count, 5);
+        }
+        #endregion
     }
 
     public class UserTests
